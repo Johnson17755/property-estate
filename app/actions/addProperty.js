f56c128eae51bf0c5a1fcac510fbd9e7e908bc2d
johnson <johnsonsynero@gmail.com>
2025-05-15 16:21:59 +0100

    feat: a new feature added

--- app/actions/addProperty.js
+++ app/actions/addProperty.js
@@ -1,18 +1,27 @@
 "use server";
-
-import { Square_Peg } from "next/font/google";
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { getSessionUser } from "@/utilis/getSessionUser";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 async function addProperty(formData) {
-  // access all values from amenities and images. handles all submission.
+  await connectDB();
+
+  const sessionUser = await getSessionUser();
+  if (!sessionUser || !sessionUser.userId) {
+    throw new Error("User ID is required");
+  }
+  const { userId } = sessionUser;
 
   const amenities = formData.getAll("amenities");
   const images = formData
     .getAll("images")
     .filter((image) => image.name !== "")
     .map((image) => image.name);
-  // console.log(images);
 
   const propertyData = {
+    owner: userId,
     type: formData.get("type"),
     name: formData.get("name"),
     description: formData.get("description"),
@@ -31,14 +40,20 @@ async function addProperty(formData) {
       weekly: formData.get("rates.weekly"),
       monthly: formData.get("rates.monthly"),
     },
-    seller_infor: {
+    seller_info: {
+      // Fixed typo: seller_infor -> seller_info
       name: formData.get("seller_info.name"),
       email: formData.get("seller_info.email"),
       phone: formData.get("seller_info.phone"),
     },
     images,
   };
-  //   console.log(propertyData);
+
+  const newProperty = new Property(propertyData);
+  await newProperty.save();
+
+  revalidatePath("/", "layout");
+  redirect(`/properties/${newProperty._id}`);
 }
 
 export default addProperty;
